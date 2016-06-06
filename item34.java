4c65d86a10ee40318b3f569e03ddabf94e6c663e
Shogo Sekine <gan.gun.kyosho@gmail.com>
2016-06-06 14:20:04 +0900

    aa

--- item34.java
+++ item34.java
@@ -60,7 +60,27 @@ public enum ExtendedOperation implements Operation{
   @Override public String toString(){
     return symbol;
   }
+}
 
+//模倣された拡張enum
+public enum ExtendedOperation implements Operation{
+  EXP("^"){
+    public double apply(double x, double y){
+      return Math.pow(x, y);
+    }
+  },
 
+  EXP("%"){
+    public double apply(double x, double y){
+      return x % y;
+    }
+  }
 
+  private final String symbol;
+  ExtendedOperation(String symbol){
+    this.symbol = symbol;
+  }
+  @Override public String toString(){
+    return symbol;
+  }
 }
